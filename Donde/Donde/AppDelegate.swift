19fbda9b2dcf15ae2aea0f182bcdb4935ccb0d37
Jesus Garnica <gtechy100@gmail.com>
2018-12-13 13:24:59 -0800

    Iniital Code Push
    I tried to push and it failed. So I lost a bunch of work. I am sure I can recover it.

---
+++ Donde/Donde/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+import GoogleMaps
+
+// 1
+let googleApiKey = "ENTER_KEY_HERE"
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        //2
+        GMSServices.provideAPIKey(googleApiKey)
+        return true
+    }
+}
+
